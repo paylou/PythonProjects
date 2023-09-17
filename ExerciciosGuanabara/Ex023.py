1ec88c3f534ed52986af2d3301a65c648341736e
Paulo Guilherme <pauloguipg1234@gmail.com>
2023-09-17 18:41:17 -0400

    Exercicio 22 resolvido

---
+++ ExerciciosGuanabara/Ex023.py
@@ -0,0 +1,10 @@
+numero = str(input("Informe um número de 0 a 9999:  ")).zfill(4)
+
+
+print(f"Unidade:   {numero[-1]}")
+print(f"Dezena:    {numero[-2]}")
+print(f"Centena:   {numero[-3]}")
+print(f"Milhar:    {numero[-4]}")
+
+print(numero)
+
