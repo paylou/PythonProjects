1cb45bb539dc3471ce0e9e381eaee33bcf72f7bb
Paulo Guilherme <pauloguipg1234@gmail.com>
2023-09-17 20:06:50 -0400

    Exersicio 024 resolvido

---
+++ ExerciciosGuanabara/Ex024.py
@@ -0,0 +1,4 @@
+cidade = str(input("Informe o nome de uma cidade:   ")).strip()
+
+print(f"A cidade '{cidade.title()}' começa com a palavra SANTO?: {cidade.upper().startswith('SANTO')}")
+            
